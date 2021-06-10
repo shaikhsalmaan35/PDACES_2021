40c54be2a297ea654ec72b4fe8fdc7695674e733
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-06-10 15:16:38 +0530

    map.method

---
+++ MapAssignment.js
@@ -0,0 +1,14 @@
+const MapAssignement =(data)=> data.map(
+    (item)=>
+     `I am planing to do ${item.course} from ${item.institute} situted at ${item.city} having contact as ${item.MobileNo}`
+)
+
+console.log(MapAssignement([
+    {
+        course: "PDAC",
+        institute : "Pickupbiz",
+        city:"Nanded",
+        MobileNo: 9874561230
+        
+    }
+]))
