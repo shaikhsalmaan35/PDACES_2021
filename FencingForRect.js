fd0a08f99e1650b7739382c8f77a3340807d7d34
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-06-14 18:52:21 +0530

    retrun-totalCost

--- FencingForRect.js
+++ FencingForRect.js
@@ -1,5 +1,7 @@
 const FencingForRect=(b,w, numberOfWire, costPerFeet)=>{
     const parimeter = b+w+b+w;
+    const totalCost = parimeter*numberOfWire*costPerFeet;
+    return totalCost;
 
 }
 console.log(`${(FencingForRect(20,30))}`);
