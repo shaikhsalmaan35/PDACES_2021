8fb32ca05922f0a5270713297ef68535136fddaf
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-06-14 18:25:22 +0530

    Bigger-changes

--- BiggerNumMultiOpt.js
+++ BiggerNumMultiOpt.js
@@ -1,11 +1,14 @@
-const BiggerNumMultiOpt=(num1,num2,num3,num4)=>{
-    let BigNum=0;
+const BiggerNumMultiOpt=(num1,num2,num3,num4,num5)=>
+    Math.mix(num1,num2,num3,num4,num5);
+//     {let BigNum=0;
 
-    BigNum = (num1>num2) ? num1 : num2;
-    BigNum = (BigNum>num3) ? BigNum:num3;
-    BigNum = (BigNum>num4) ? BigNum : num4;
+//      BigNum = (num1>num2) ? num1 : num2;
+//      BigNum = (BigNum>num3) ? BigNum:num3;
+//      BigNum = (BigNum>num4) ? BigNum : num4;
+//      BigNum = (BigNum>num5) ? BigNum : num5;
 
-    return BigNum;
-};
-console.log(`Bigger Number is ${BiggerNumMultiOpt(25,56,78,28)}`);
+//      return BigNum;
+// };
+
+console.log(`Bigger Number is ${BiggerNumMultiOpt(25,56,78,28,109)}`);
 
