7a7318ed1b17755fba8e823972caee323a2806a6
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-06-30 13:18:33 +0530

    console-print

--- add.js
+++ add.js
@@ -1,6 +1,13 @@
 function addme(num1,num2){
     var sum=num1+num2;
-    console.log("Sum of " + num1,"and" + num2 + "is equal to " + sum);
+    // console.log("Sum of " + num1,"and" + num2 + "is equal to " + sum);
+
+    console.log(`sum of ${num1} and ${num2} is equal to ${sum}`);
 }
 addme(23,75);
 addme(45,45);
+// function add(num1,num2){
+//     var sum=num1+num2;
+//     console.log(sum=num1+num2);
+// }
+// add(11,25);
