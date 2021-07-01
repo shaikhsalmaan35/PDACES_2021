7983f3833a276171a92e7876b6c700010c0db958
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-01 12:29:37 +0530

    create-three-function

--- call.js
+++ call.js
@@ -2,3 +2,12 @@ function callMe(){
     console.log("Thanks for call !!!!");
 }
 callMe();
+
+function call(){
+    console.log("Hey call this function");
+}
+call();
+function call1(){
+    console.log("This is a call Ecma");
+}
+call1();
