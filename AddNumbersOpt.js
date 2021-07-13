67841fa1fc932b799c17ce97e153319020f28324
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-13 12:55:46 +0530

    Backtics-uesd-

--- AddNumbersOpt.js
+++ AddNumbersOpt.js
@@ -1,7 +1,12 @@
-function AddNumbersOpt(num1,num2){
-    const sum = num1 + num2 ;
-    return sum;
-}
+const AddNumbersOpt = (num1 , num2) => num1 +num2;
+const SubNumbersOpt = (num1 , num2) => num1 - num2;
+
 console.log(AddNumbersOpt(3,5));
 
 console.log("sum of  given numbers are" + AddNumbersOpt(15,10));
+
+console.log(`=========
+Sum of 
+30 + 60 = ${AddNumbersOpt(30 , 60)}
+30 - 60 = ${SubNumbersOpt(30 , 60)}
+=========================`)
