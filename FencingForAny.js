371b33b0949ca4e1ae32b9015610d6b673be92f1
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-16 12:38:44 +0530

    complete-Done-FencingForAny-Arry

--- FencingForAny.js
+++ FencingForAny.js
@@ -1,3 +1,9 @@
-const FencingForAny=()={
-    
-}
+const FencingForAny=(edges,numerOfWire,rate)=>{
+    let sum=0;
+    for(let i=0;i<edges.length;i++){
+        sum = sum + edges[i];
+
+    }
+    return sum;
+};
+console.log(FencingForAny ([10,15,10,10,10,10,22],5,10));
