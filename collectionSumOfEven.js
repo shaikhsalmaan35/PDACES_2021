206c5e215b439ec12aecf35cfa2596115513c693
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-17 13:05:03 +0530

    three way of collection sum of even

--- collectionSumOfEven.js
+++ collectionSumOfEven.js
@@ -20,7 +20,18 @@ const collectionSumOfEven=(number)=>{
 console.log(collectionSumOfEven([5,2,7,9,10,8,17,115,10]));
 
 // ----------------------------------Even ANd Odd----------------------------------------
-
+    const collectionSumOfEven =(number)=>{
+        let sunEven =0;
+        let sumOdd =0;
+        number.forEach((element)=>{
+            element % 2 == 0
+            ? (sumEven = sumEven+element)
+            :(sumOdd = sum+sumOdd);
+        });
+        return `sum of even Number =${sumEven} and
+        sum of odd Number = ${sumOdd}
+        Total `
+    }
 
 
 
