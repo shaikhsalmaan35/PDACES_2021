569f33a1d90833349a83dbdb9ed5a6b6f85aa7b3
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-17 12:07:14 +0530

    Odd-no

--- collectionSumOfEven.js
+++ collectionSumOfEven.js
@@ -1,8 +1,26 @@
+// --------------------OddNumber--------------------------
 const collectionSumOfEven=(number)=>{
     let sum=0;
     number.forEach((element)=>{
-        sum=sum+element;
+        element%2===1 && (sum=sum+element);
+        
     });
     return sum;
 }
 console.log(collectionSumOfEven([5,2,7,9,10,8,17,115,10]));
+
+// -----------------------EvenNO------------------------------
+
+const collectionSumOfEven=(number)=>{
+    let sum=0;
+    number.forEach((element)=>{
+        element % 2 === 0 && (sum=sum+element);
+    });
+}
+console.log(collectionSumOfEven([5,2,7,9,10,8,17,115,10]));
+
+// ----------------------------------Even ANd Odd----------------------------------------
+
+
+
+
