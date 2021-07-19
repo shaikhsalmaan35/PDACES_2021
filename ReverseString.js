866b0deb3e4671bb3eef9d7dda440f28ced29540
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-20 00:55:54 +0530

    ReverseString-done

--- ReverseString.js
+++ ReverseString.js
@@ -1,5 +1,9 @@
-const revString = (str) => {
-  const arrStr = str.split("");
+const revStr = (str) => {
+  const arrStr = str.split(" ");
   const newArr = [];
-  for (i = arrStr.length - 1; i >= 0; i--) {}
+  for (i = arrStr.length - 1; i >= 0; i--) {
+    newArr.push(arrStr[i]);
+  }
+  return newArr.join(" ");
 };
+console.log(revStr("I AM Fine"));
