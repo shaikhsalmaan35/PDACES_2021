993ed4e9de0c6a47a56f743e908ad695f7fd3f9e
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-07-22 01:20:44 +0530

    Number-also-remove-this-log

--- RemoveDuplicateFromString.js
+++ RemoveDuplicateFromString.js
@@ -4,4 +4,4 @@ const RemoveDuplicateFromString = (str) => {
   return uniqueArr.join(" ");
 };
 
-console.log(RemoveDuplicateFromString("I am Good. I want to go . I am fine "));
+console.log(RemoveDuplicateFromString("1 2 5 3 5 8 7 8 9 1 2 3 5 4 4 5 6  "));
