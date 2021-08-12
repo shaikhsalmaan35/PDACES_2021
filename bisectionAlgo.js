3a5db8ddf26e117721827f2de4dc46126436bcb1
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-12 15:09:41 +0530

    bisectionAlgo

--- bisectionAlgo.js
+++ bisectionAlgo.js
@@ -1,3 +1,41 @@
-const bisetionAlgo =()=>{
-    
+let firstIndex = 0;
+let lastIndex = 0;
+const findExist = (arrNum, findNum) => {
+    const mid = Math.floor((firstIndex + lastIndex) / 2);
+    if (arrNum[mid] === findNum) {
+        return mid;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const bisetionAlgo = (arrNum, findNum) => {
+//     let firstIndex = 0;
+//     let lastIndex = arrNum.length - 1;
+//     let mid = math.floor((firstIndex + lastIndex) / 2);
+//     if (arrNum[mid] === findNum) {
+//         return mid;
+//     } else {
+//         lastIndex = mid;
+//     }
+// }
+
+// console.log(bisetionAlgo([2, 4, 5, 7, 8, 11, 45, 77, 120, 120, 145], 120));
