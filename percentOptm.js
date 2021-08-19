5bfe18bced418e6c42a454b1a5bc2f39efb98702
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-19 12:35:59 +0530

    MarksOptm-way-reduce-method

--- percentOptm.js
+++ percentOptm.js
@@ -1,7 +1,18 @@
-const percent = (Number) => Number.reduce((a, b) => a + b, 0) / Number.length;
+// const marks = (Number) => Number.reduce((a, b) => a + b, 0) / Number.length;
 
-console.log(percent([45, 25, 85, 75, 48]));
+// console.log(percent([45, 25, 85, 75, 48]));
 
-console.log(percent([48, 78, 96, 48, 75]));
+// console.log(percent([48, 78, 96, 48, 75]));
 
-console.log(percent([35, 56, 87, 95, 78, 58, 79, 78, 99]));
+// console.log(percent([35, 56, 87, 95, 78, 58, 79, 78, 99]));
+
+// ---------Optm way-------Sir Method------
+
+const marks = (numbers) =>
+  numbers.reduce((acc, curr) => acc + curr) / numbers.length;
+
+console.log(marks([45, 25, 85, 75, 48]));
+
+console.log(marks([48, 78, 96, 48, 75]));
+
+console.log(marks([35, 56, 87, 95, 78, 58, 79, 78, 99]));
