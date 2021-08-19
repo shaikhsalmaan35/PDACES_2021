e4bab2887edd2069c809a691af871457ff1d4e11
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-19 12:26:05 +0530

    percentOptm-way

---
+++ percentOptm.js
@@ -0,0 +1,7 @@
+const percent = (Number) => Number.reduce((a, b) => a + b, 0) / Number.length;
+
+console.log(percent([45, 25, 85, 75, 48]));
+
+console.log(percent([48, 78, 96, 48, 75]));
+
+console.log(percent([35, 56, 87, 95, 78, 58, 79, 78, 99]));
