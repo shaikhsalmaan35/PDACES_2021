8ca0fdea8383d044fa8f1db7521368df220f3cbd
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-21 15:29:15 +0530

    Number-

--- DataCheck.js
+++ DataCheck.js
@@ -1,5 +1,11 @@
 // -------Anser----Number
-console.log(typeof 1);
+// console.log(typeof 1);
 
-// -------String----answer
+// ---Answer----String----
 // console.log(typeof typeof 1);
+
+// -----Anser---(11)-------
+// console.log(1 + "1");
+
+// -------Answer-------2----
+// console.log(1 + 1);
