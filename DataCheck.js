2c4ca411b992fb51e3fb2779e36dfed01fdd8123
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-21 15:43:38 +0530

    const-or-var-diffrent

--- DataCheck.js
+++ DataCheck.js
@@ -1,3 +1,26 @@
+// ------const use karemge toh over right---Identifier 'getName' has already been declared
+// const getName = (fname, lname) => {
+//   return fname + " " + lname;
+// };
+// const getName = (fname, mname, lname) => {
+//   return fname + " " + mname + " " + lname;
+// };
+// console.log(getName("salman", "shaikh"));
+// console.log(getName("salman", "shaikh", "sam"));
+
+//-----agar var use karenge toh second function call------
+//-----Answer------1st line :-salman shaikh undefined
+// 2nd line :- salman shaikh sam
+
+var getName = (fname, lname) => {
+  return fname + " " + lname;
+};
+var getName = (fname, mname, lname) => {
+  return fname + " " + mname + " " + lname;
+};
+console.log(getName("salman", "shaikh"));
+console.log(getName("salman", "shaikh", "sam"));
+
 // -------Anser----Number
 // console.log(typeof 1);
 
