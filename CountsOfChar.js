a22ff92c5d0b50af0260b4feccb4fccc62bf062c
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-24 12:07:06 +0530

    CountOfChar

---
+++ CountsOfChar.js
@@ -0,0 +1,17 @@
+const CountsOfChar = (str) => {
+  const arrStr = str.split("");
+  const newObj = {};
+
+  // console.log("before-----", newObj["a"]);  //-----undefined
+
+  // newObj["a"] = 5;
+
+  // console.log("after-----", newObj["a"]++);  //-----acess to element
+
+  arrStr.forEach((element) => {
+    newObj[element] === undefined ? (newObj[element] = 1) : newObj[element]++;
+  });
+
+  return newObj;
+};
+console.log(CountsOfChar("mine of success is dependnon your success"));
