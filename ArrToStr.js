26cfd1837b9390229c4c15b6da0629ce8da3d1e3
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-26 16:48:42 +0530

    object-method

--- ArrToStr.js
+++ ArrToStr.js
@@ -1,6 +1,6 @@
 const arr = [null, 2, 3, 4, "cat", { num: 25 }, undefined, 2];
 
 const result = arr
-  .filter((item) => typeof item === "string" && item)
+  .filter((item) => typeof item === "object" && item)
   .reduce((acc, num) => acc + num);
 console.log(result);
