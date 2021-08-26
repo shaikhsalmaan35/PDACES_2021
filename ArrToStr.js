6e77c40ab896057e2bc5498b64f5d11456a7a748
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-26 16:47:14 +0530

    arrToStr

---
+++ ArrToStr.js
@@ -0,0 +1,6 @@
+const arr = [null, 2, 3, 4, "cat", { num: 25 }, undefined, 2];
+
+const result = arr
+  .filter((item) => typeof item === "string" && item)
+  .reduce((acc, num) => acc + num);
+console.log(result);
