9ebc9e58a445ba5a8ade334c35933d46c052d492
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-26 17:09:14 +0530

    typeof

--- ArrToStr.js
+++ ArrToStr.js
@@ -1,6 +1,6 @@
 const arr = [null, 2, 3, 4, "cat", { num: 25 }, undefined, 2];
 
 const result = arr
-  .filter((item) => typeof item === "string" && item)
+  .filter((item) => typeof item === "number" && item) //any typeof data type 1.number 2.string 3.object
   .reduce((acc, num) => acc + num);
 console.log(result);
