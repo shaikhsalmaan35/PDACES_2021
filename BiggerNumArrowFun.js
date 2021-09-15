f8aa975c5d6f4f26765000b24d3c09439c79a0c5
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-09-15 15:44:18 +0530

    BiggerNumArrowFun

--- BiggerNumArrowFun.js
+++ BiggerNumArrowFun.js
@@ -1,5 +1,5 @@
 // const bigMulti = (num1, num2, num3, num4) => Math.max(num1, num2, num3, num4);
 // console.log(bigMulti(24, 12, 15, 48));
 
-const BigNum = (num1, num2) => (num1 > num2 ? num1 : num2);
-console.log(BigNum(15, 23));
+// const BigNum = (num1, num2) => (num1 > num2 ? num1 : num2);
+// console.log(BigNum(15, 23));
---
+++ ObjToArrValues.js
@@ -0,0 +1,8 @@
+const obj = {
+  name: "shaikh",
+  city: "Nanded",
+  contact: 7887884455,
+};
+
+const valuesArr = object.entries(obj);
+console.log(valuesArr);
