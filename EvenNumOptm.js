0a66e2b3635eeeb1ee7635b354ce2c7db8f5a65a
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-09-15 16:41:42 +0530

    evenNo.

---
+++ EvenNumOptm.js
@@ -0,0 +1,4 @@
+const evenNum = (n1) => {
+  return n1 % 2 === 0 ? "even No" : "Odd No";
+};
+console.log(evenNum(74587));
---
+++ evenNum.js
@@ -0,0 +1,8 @@
+const evenNum = (n1) => {
+  if (n1 % 2 === 0) {
+    return "Even No";
+  } else {
+    return "Odd No.";
+  }
+};
+console.log(evenNum(6));
