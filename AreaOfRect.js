1a6ccfc8bae054d4c6b0323d6129d4bc12b99055
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-09-15 16:04:42 +0530

    hoisting-concept

--- AreaOfRect.js
+++ AreaOfRect.js
@@ -1,3 +1,8 @@
 const areaOfRect = (l, b) => l * b;
-
-console.log(areaOfRect(150, 50));
+//hoisting
+result = areaOfRect(150, 50);
+result = areaOfRect(40, 50);
+result = areaOfRect(10, 50);
+result = areaOfRect(70, 50);
+var result;
+console.log(result);
